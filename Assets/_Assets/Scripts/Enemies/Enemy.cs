5c14c24ca1f5b8da3de0dc990d2779a20318ce81
vicgb-dev <93768447+vicgb-dev@users.noreply.github.com>
2025-08-23 19:13:12 +0200

    Unity 6

--- Assets/_Assets/Scripts/Enemies/Enemy.cs
+++ Assets/_Assets/Scripts/Enemies/Enemy.cs
@@ -40,7 +40,7 @@ public abstract class Enemy : MonoBehaviour
 	public virtual void StopMoving(bool win = true)
 	{
 		rb.isKinematic = true;
-		rb.velocity = Vector2.zero;
+		rb.linearVelocity = Vector2.zero;
 		coll.enabled = false;
 		stopped = true;
 		//Debug.Log($"$rb.isKinematic = {rb.isKinematic} | rb.velocity = {rb.velocity} | coll.enabled = {coll.enabled} | stopped = {stopped}");
